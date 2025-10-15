acfb47a9ae04b17144c6fb1adf72579a733d8d34
Ashitom <sasukeuchia32828@gmail.com>
2025-10-15 19:03:28 +0545

    Initial Commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/moby/moby/client"
+	"github.com/moby/moby/api/types/container"
+
+)
+
+func main(){
+	fmt.Printf("Lambda System")
+}
+
